0a6b57e96acc8e81cb5ab349737286aa0669e407
ysl <yangshuoliu@sina.com>
2018-03-13 16:33:10 +0800

    .

--- PathTracingDemo.h
+++ PathTracingDemo.h
@@ -4,9 +4,15 @@
 #include "basedemowidget.h"
 #include "openglwindow.h"
 #include <QVector3D>
+#include <cassert>
+
+#include <numeric>
 
 
 typedef QVector3D Point3f;
+
+constexpr Float MIN_Float_VALUE = std::numeric_limits<Float>::lowest();
+constexpr Float MAX_Float_VALUE = std::numeric_limits<Float>::max();
 
 /*
  *
@@ -19,7 +25,7 @@ class Ray
     Float m_t;
 public:
     Ray(const Point3f & d, const Point3f & o, Float t) :m_o(o), m_d(d), m_t(t) {}
-    QVector3D operator()(float t) { return m_o + t * m_d; }
+    Point3f operator()(float t) { return m_o + t * m_d; }
     friend class AABB3;
 };
 
@@ -27,34 +33,56 @@ public:
  * Axis-aligned Bounding Box
  */
 
+
+
 class AABB
 {
     Point3f m_min;
     Point3f m_max;
 public:
-    AABB(const Point3f & p0 = Point3f(),const Point3f & p1 = Point3f()):m_min(p0),m_max(p1){}
+    AABB(const Point3f & p0 = Point3f(MIN_Float_VALUE,MIN_Float_VALUE,MIN_Float_VALUE),
+         const Point3f & p1 = Point3f(MAX_Float_VALUE,MAX_Float_VALUE,MAX_Float_VALUE)):
+        m_min(std::min(p0.x(),p1.x()),std::min(p0.y(),p1.y()),std::min(p0.z(),p1.z())),
+        m_max(std::max(p0.x(),p1.x()),std::max(p0.y(),p1.y()),std::max(p0.z(),p1.z()))
+    {}
     AABB(const Point3f & p):m_min(p),m_max(p){}
-    Point3f corner(int i){
 
+    const Point3f & operator[](int i)const{
+        assert(i>=0&&i<2);
+        if(i==0)return m_min;
+        if(i==1)return m_max;
+        return Point3f();
     }
 
-    bool intersect(const Ray & ray, Float * t)
+    Point3f & operator[](int i){
+        return const_cast<Point3f>(static_cast<const AABB>(*this)[i]);
+    }
+
+    Point3f corner(int i)const{
+        return Point3f(
+                    (*this)[i&1].x(),(*this)[i&2].y(),(*this)[i&4].z()
+                    );
+    }
+
+    //whether this intersect with a ray
+    bool intersect(const Ray & ray, Float * t = nullptr)const
     {
         
     }
-
-
-    static AABB intersect(const AABB & b0,const AABB & b1){
-
-    }
-    static AABB unionWith(const AABB & b,const Point3f & p){
+    //a point is inside the bounding box
+    bool inside(const Point3f & p)const{
 
     }
 
-    static bool inside(const AABB & b,const Point3f & p){
+    AABB overlap(const AABB & b)const{
 
     }
+    AABB unionWith(const AABB & b)const{
 
+    }
+    AABB unionWith(const Point3f & p)const{
+
+    }
 };
 
 class Shape
