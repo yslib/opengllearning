ed4801533f67e913807cf22d447b67ea78193683
ysl <yangshuoliu@sina.com>
2018-04-19 23:03:31 +0800

    .

--- kdtree.h
+++ kdtree.h
@@ -7,6 +7,19 @@
 class KDTreeAccelerator:public Shape
 {
 	std::vector<std::shared_ptr<Shape>> m_shapes;
+    struct KDTreeNode{
+
+        union{
+            Float split;
+            int onePrimtive;
+            int primitiveIndicesOffset;
+        };
+        union{
+            int flag;
+            int nPrim;
+            int aboveChild;
+        };
+    };
 
 public:
 	KDTreeAccelerator(std::vector<std::shared_ptr<Shape>> & shapes):Shape(nullptr) {}
